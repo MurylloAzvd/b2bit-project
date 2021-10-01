ebbaa1794f0ce8d5d2fac383adebe752c2703e19
Muryllo <muryllo.dev@gmail.com>
2021-10-01 11:48:00 -0300

    formik

--- src/App.js
+++ src/App.js
@@ -1,10 +1,69 @@
+import React from "react";
+import { Formik } from "formik";
+import * as Yup from "yup";
 
 function App() {
+
   return (
-    <div className="App">
-      aaaaaaa
+    <div>
+
+      <Formik
+        initialValues={{ email: "" }}
+        onSubmit={values => {
+          console.log('submit', values)
+        }}
+        validationSchema={Yup.object().shape({
+          email: Yup.string()
+            .email("estilo email")
+            .required("obrigatório mano")
+        })}
+      >
+        {({
+          values,
+          touched,
+          errors,
+          dirty,
+          isSubmitting,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          handleReset
+        }) => (
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="email" style={{ display: "block" }}>
+              Email
+            </label>
+            <input
+              id="email"
+              placeholder="Enter your email"
+              type="text"
+              value={values.email}
+              onChange={handleChange}
+              onBlur={handleBlur}
+            />
+            {errors.email && touched.email && (
+              <div>{errors.email}</div>
+            )}
+
+            <button
+              type="button"
+              onClick={handleReset}
+              disabled={!dirty || isSubmitting}
+            >
+              Reset
+            </button>
+            <button type="submit" disabled={isSubmitting}>
+              Submit
+            </button>
+
+
+          </form>
+        )}
+      </Formik>
+
     </div>
   );
+
 }
 
 export default App;
