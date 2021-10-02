35cefc5ee49bec33ecf121ee9e44ea8eda7de04a
Muryllo <muryllo.dev@gmail.com>
2021-10-02 18:01:57 -0300

    finished new style

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,18 @@
 import { logout } from "../services/auth";
+import b2bitLogo from '../assets/b2bit-logo.png'
 
 function Home() {
 
     return (
-        <>
-            <h1>Você foi logado com sucesso!</h1>
-            <button onClick={() => {
-                logout();
-            }}>Logout</button>
-        </>
+        <div className="h-screen flex flex-col items-center justify-center">
+            <img src={b2bitLogo} alt="Logo" />
+            <h1 className="text-white font-bold text-4xl mb-8 text-center">Parabéns, você foi logado com sucesso!</h1>
+            <button
+                className="bg-blue-500 hover:bg-blue-dark text-white font-bold py-2 px-6 rounded"
+                onClick={() => {
+                    logout();
+                }}>Sair</button>
+        </div>
     )
 }
 
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,79 +1,108 @@
-import React from "react";
+import React, { useState } from "react";
 import { Formik } from "formik";
 import * as Yup from "yup";
 import api from "../services/api";
 import { login } from "../services/auth";
 import { useHistory } from "react-router-dom";
+import b2bitLogo from '../assets/b2bit-logo.png'
 
 function Login() {
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
+
     const history = useHistory();
 
     return (
-        <Formik
-            initialValues={{ email: "", password: "" }}
-            onSubmit={values => {
-                api
-                    .post("/auth/token/", values)
-                    .then((res) => {
-                        login(res.data.tokens.access);
-                        history.push("/");
-                    })
-                    .catch((err) => console.log(err))
-            }}
-            validationSchema={Yup.object().shape({
-                email: Yup.string()
-                    .email('Campo inválido')
-                    .required('Campo obrigatório'),
-                password: Yup.string()
-                    .min(6, 'Senha deve ter no mínimo 6 dígitos')
-                    .required('Campo obrigatório'),
-            })}
-        >
-            {({
-                values,
-                touched,
-                errors,
-                handleChange,
-                handleBlur,
-                handleSubmit,
-            }) => (
-                <form onSubmit={handleSubmit}>
-                    <label htmlFor="email" style={{ display: "block" }}>
-                        Email
-                    </label>
-                    <input
-                        id="email"
-                        placeholder="Enter your email"
-                        type="email"
-                        value={values.email}
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                    />
-                    {errors.email && touched.email && (
-                        <div>{errors.email}</div>
-                    )}
+        <div className="flex flex-col md:flex-row justify-evenly items-center h-screen px-6">
+            <img src={b2bitLogo} alt="Logo" className="mt-12 md:mt-0" />
+            <Formik
+                initialValues={{ email: "", password: "" }}
+                onSubmit={values => {
+                    setLoading(true);
+                    api
+                        .post("/auth/token/", values)
+                        .then((res) => {
+                            setLoading(false);
+                            setError(false);
+                            login(res.data.tokens.access);
+                            history.push("/");
+                        })
+                        .catch((err) => {
+                            setLoading(false);
+                            setError(true);
+                            console.log(err);
+                        })
+                }}
+                validationSchema={Yup.object().shape({
+                    email: Yup.string()
+                        .email('Campo inválido')
+                        .required('Campo obrigatório'),
+                    password: Yup.string()
+                        .min(6, 'Senha deve ter no mínimo 6 dígitos')
+                        .required('Campo obrigatório'),
+                })}
+            >
+                {({
+                    values,
+                    touched,
+                    errors,
+                    handleChange,
+                    handleBlur,
+                    handleSubmit,
+                }) => (
+                    <form onSubmit={handleSubmit} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col">
+                        <div className="mb-4">
+                            <label htmlFor="email" className="block text-grey-darker text-sm font-bold mb-2">
+                                Email
+                            </label>
+                            <input
+                                id="email"
+                                placeholder="Digite seu email"
+                                type="email"
+                                value={values.email}
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker mb-3"
+                            />
+                            {errors.email && touched.email && (
+                                <p className="text-red-600 text-xs italic">{errors.email}</p>
+                            )}
+                        </div>
 
-                    <label htmlFor="password" style={{ display: "block" }}>
-                        Password
-                    </label>
-                    <input
-                        id="password"
-                        placeholder="Enter your password"
-                        type="password"
-                        value={values.password}
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                    />
-                    {errors.password && touched.password && (
-                        <div>{errors.password}</div>
-                    )}
+                        <div className="mb-6">
+                            <label htmlFor="password" className="block text-grey-darker text-sm font-bold mb-2">
+                                Senha
+                            </label>
+                            <input
+                                id="password"
+                                placeholder="Digite sua senha"
+                                type="password"
+                                value={values.password}
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                className="shadow appearance-none border border-red rounded w-full py-2 px-3 text-grey-darker mb-3"
+                            />
+                            {errors.password && touched.password && (
+                                <p className="text-red-600 text-xs italic">{errors.password}</p>
+                            )}
+                        </div>
 
-                    <button type="submit">
-                        Submit
-                    </button>
-                </form>
-            )}
-        </Formik>
+                        <button type="submit" className="bg-blue-500 hover:bg-blue-dark text-white font-bold py-2 px-4 rounded flex items-center justify-center">
+                            {
+                                loading ?
+                                    <div className="animate-spin rounded-full h-6 w-6 border-b-2 border-gray-900" />
+                                    :
+                                    'Entrar'
+                            }
+                        </button>
+                        {
+                            error && <p className="text-red-600 text-sm text-center mt-6">Email ou senha incorretos. Tente novamente!</p>
+                        }
+                    </form>
+                )}
+            </Formik>
+        </div>
+
     );
 
 }
